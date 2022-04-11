7d0ca07c8ffe24f4d34f5d995990924d45030df5
Your Name <you@example.com>
2022-04-11 13:08:23 +0300

    edit routes and controllers

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import express, { Application, Request, Response } from "express";
 import mongoose from 'mongoose';
+import { router } from './routes/task'
 
 require('dotenv').config();
 
 const port: number | string = process.env.PORT || 3000;
 const app: Application = express();
 
-
 // Settings app
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ limit: '1.5MB' }))
 
-mongoose.connect(process.env.MONGO_URL|| "")
+// Settings DB
+mongoose.connect(process.env.MONGO_URL || "")
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('ok')
-});
+// Settings routes
+app.use('/api/tasks', router)
 
-// RUN SERVER
+// Run server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
