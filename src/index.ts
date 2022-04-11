b8c02dacb2cb597ccf06f8a925899c9b2e91d90d
Your Name <you@example.com>
2022-04-11 12:43:23 +0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from "express";
+import mongoose from 'mongoose';
+
+require('dotenv').config();
+
+const port: number | string = process.env.PORT || 3001;
+const app: Application = express();
+
+
+// Settings app
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '1.5MB' }))
+
+mongoose.connect(process.env.MONGO_URL|| "")
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('ok')
+});
+
+// RUN SERVER
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
